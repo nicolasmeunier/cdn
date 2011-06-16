ed4b0498011693cbb3600a318069810ddad92710
Nicolas Meunier <nmeunie@gmail.com>
2011-06-16 13:34:27 -0700

    Adding s3sync to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Cdn::Application.initialize!
+
+config.load_paths << "#{RAILS_ROOT}/vendor/plugins/synch_s3_asset_host/s3sync/"
